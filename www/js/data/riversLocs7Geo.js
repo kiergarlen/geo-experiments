a82067d2a8d17bce4083209300592a3afd463910
kiergarlen <oscar_igm@hotmail.com>
2014-06-05 11:09:21 -0500

    Error fixing

--- www/js/data/riversLocs7Geo.js
+++ www/js/data/riversLocs7Geo.js
@@ -1,3 +1,9 @@
 var riversLocs7Geo = {"type":"FeatureCollection","features":[
-	{"type":"Feature","properties":{"id":140530001,"name":"Lagos de Moreno","p":98206,"lv":7},"geometry":{"type":"Point","coordinates":[-101.929167,21.356388]}},{"type":"Feature","properties":{"id":140930001,"name":"Tepatitlán de Morelos","p":91959,"lv":7},"geometry":{"type":"Point","coordinates":[-102.757223,20.8175]}},{"type":"Feature","properties":{"id":140970822,"name":"Hacienda Santa Fe","p":86935,"lv":7},"geometry":{"type":"Point","coordinates":[-103.380556,20.518055]}},{"type":"Feature","properties":{"id":140630001,"name":"Ocotlán","p":83769,"lv":7},"geometry":{"type":"Point","coordinates":[-102.768056,20.351388]}},{"type":"Feature","properties":{"id":141200231,"name":"Tesistán (San Francisco Tesistán)","p":62397,"lv":7},"geometry":{"type":"Point","coordinates":[-103.486112,20.798333]}},{"type":"Feature","properties":{"id":140080001,"name":"Arandas","p":52175,"lv":7},"geometry":{"type":"Point","coordinates":[-102.340278,20.705555]}}
+{"type":"Feature","properties":{"name":"Lagos de Moreno","id":140530001,"p":98206,"lv":7},"geometry":{"type":"Point","coordinates":[-101.929167,21.356388]}},
+{"type":"Feature","properties":{"name":"Ciudad Guzmán","id":140230001,"p":97750,"lv":7},"geometry":{"type":"Point","coordinates":[-103.464167,19.708611]}},
+{"type":"Feature","properties":{"name":"Tepatitlán de Morelos","id":140930001,"p":91959,"lv":7},"geometry":{"type":"Point","coordinates":[-102.757223,20.8175]}},
+{"type":"Feature","properties":{"name":"Hacienda Santa Fe","id":140970822,"p":86935,"lv":7},"geometry":{"type":"Point","coordinates":[-103.380556,20.518055]}},
+{"type":"Feature","properties":{"name":"Ocotlán","id":140630001,"p":83769,"lv":7},"geometry":{"type":"Point","coordinates":[-102.768056,20.351388]}},
+{"type":"Feature","properties":{"name":"Tesistán (San Francisco Tesistán)","id":141200231,"p":62397,"lv":7},"geometry":{"type":"Point","coordinates":[-103.486112,20.798333]}},
+{"type":"Feature","properties":{"name":"Arandas","id":140080001,"p":52175,"lv":7},"geometry":{"type":"Point","coordinates":[-102.340278,20.705555]}}
 ]};
--- www/js/data/riversLocs8Geo.js
+++ www/js/data/riversLocs8Geo.js
@@ -1,3 +1,7 @@
 var riversLocs8Geo = {"type":"FeatureCollection","features":[
-	{"type":"Feature","properties":{"id":140390001,"name":"Guadalajara","p":1495182,"lv":8},"geometry":{"type":"Point","coordinates":[-103.342223,20.676388]}},{"type":"Feature","properties":{"id":141200001,"name":"Zapopan","p":1142483,"lv":8},"geometry":{"type":"Point","coordinates":[-103.388334,20.720555]}},{"type":"Feature","properties":{"id":140980001,"name":"Tlaquepaque","p":575942,"lv":8},"geometry":{"type":"Point","coordinates":[-103.307223,20.638611]}},{"type":"Feature","properties":{"id":141010001,"name":"Tonalá","p":408759,"lv":8},"geometry":{"type":"Point","coordinates":[-103.245556,20.623333]}}
+	{"type":"Feature","properties":{"name":"Guadalajara","id":140390001,"p":1495182,"lv":8},"geometry":{"type":"Point","coordinates":[-103.342223,20.676388]}},
+	{"type":"Feature","properties":{"name":"Zapopan","id":141200001,"p":1142483,"lv":8},"geometry":{"type":"Point","coordinates":[-103.388334,20.720555]}},
+	{"type":"Feature","properties":{"name":"Tlaquepaque","id":140980001,"p":575942,"lv":8},"geometry":{"type":"Point","coordinates":[-103.307223,20.638611]}},
+	{"type":"Feature","properties":{"name":"Tonalá","id":141010001,"p":408759,"lv":8},"geometry":{"type":"Point","coordinates":[-103.245556,20.623333]}},
+	{"type":"Feature","properties":{"name":"Puerto Vallarta","id":140670001,"p":203342,"lv":8},"geometry":{"type":"Point","coordinates":[-105.227223,20.613611]}}
 ]};
